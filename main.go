7d185e8ea54c8336ad4331c056b655ea39258471
cocap10 <martin.piegay@zenika.com>
2016-07-04 09:15:50 +0000

    step 3

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 )
 
+func fizzbuzz(number int) (msg string) {
+	if number%5 == 0 {
+		msg = "fizz"
+	}
+	if number%7 == 0 {
+		msg += "buzz"
+	}
+	return
+}
+
 func main() {
 	for i := 0; i < 100; i++ {
-		var message string
-		if i%5 == 0 {
-			message = "fizz"
-		}
-		if i%7 == 0 {
-			message += "buzz"
-		}
-		fmt.Printf("%d %s\n", i, message)
+		fmt.Printf("%d %s\n", i, fizzbuzz(i))
 	}
 }
